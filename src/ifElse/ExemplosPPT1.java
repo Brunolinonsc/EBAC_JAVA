9d84d7ce28f5cc294805b07a1438b91f8cbdd3ef
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-04-24 19:44:37 -0300

    ok

---
+++ src/ifElse/ExemplosPPT1.java
@@ -0,0 +1,14 @@
+package ifElse;
+/**
+ * @author bruno.lino
+ */
+public class ExemplosPPT1 {
+    public static void main(String[] args) {
+        int result = 0;
+        if (result > 1){
+            System.out.println("Resultado maior que 1.");
+        } else {
+            System.out.println("Resultado menor que 1. ");
+        }
+    }
+}
