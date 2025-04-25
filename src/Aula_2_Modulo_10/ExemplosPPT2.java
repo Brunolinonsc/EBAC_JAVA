78ef567af6b5b0f1ed1ac35fa302a0c36ea21a70
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-04-25 17:39:13 -0300

    ok

---
+++ src/Aula_2_Modulo_10/ExemplosPPT2.java
@@ -0,0 +1,10 @@
+package Aula_2_Modulo_10;
+
+public class ExemplosPPT2 {
+    public static void main(String[] args) {
+        for (int contador=1; contador<=1000; contador ++){
+            System.out.println("Esta é a repetição nr: "+contador);
+            if (contador==1000) break;
+        }
+    }
+}
