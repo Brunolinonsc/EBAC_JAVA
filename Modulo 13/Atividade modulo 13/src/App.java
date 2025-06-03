4756370f7edbb798252aeb1066ac4d4b793aabcf
BRUNO LINO NASCIMENTO <brunolinonascimento1997@gmail.com>
2025-06-03 20:36:51 -0300

    ok

--- Modulo 13/Atividade modulo 13/src/App.java
+++ Modulo 13/Atividade modulo 13/src/App.java
@@ -1,3 +1,55 @@
 public class App {
-    
+    // Classe abstrata Pessoa
+public abstract class Pessoa {
+    protected String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
+// Classe concreta PessoaFisica
+public class PessoaFisica extends Pessoa {
+    private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        super(nome);
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
+
+// Classe concreta PessoaJuridica
+public class PessoaJuridica extends Pessoa {
+    private String cnpj;
+
+    public PessoaJuridica(String nome, String cnpj) {
+        super(nome);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
+
 }
